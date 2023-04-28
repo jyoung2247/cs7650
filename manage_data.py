dfe731d17bca6fcc6924f9d06aabc48e0fe9faf7
Jacob Young <jyoung325@gatech.edu>
2023-04-28 17:49:39 -0400

    Added notebooks and deleted dataset because it was too big for github. Can regenerate with manage_data.py

--- manage_data.py
+++ manage_data.py
@@ -46,10 +46,77 @@ def create_dataset_small(input_filepath, output_filepath, dataset_len):
                     idx+=1
                else:
                     break
+def get_score_distribution(input_filepath, output_filepath):
+      with jsonlines.open(input_filepath) as reader:
+          writer = jsonlines.open(output_filepath, mode='w')
+          distributions = {}
+          distributions['-101_to_-inf'] = 0
+          distributions['-100_to_-21'] = 0
+          distributions['-20_to_-11'] = 0
+          distributions['-10_to_-6'] = 0
+          distributions['-5_to_-1'] = 0
+          distributions['0'] = 0
+          distributions['1'] = 0
+          distributions['2'] = 0
+          distributions['3'] = 0
+          distributions['4'] = 0
+          distributions['5'] = 0
+          distributions['6_to_10'] = 0
+          distributions['11_to_50'] = 0
+          distributions['51_to_100'] = 0
+          distributions['101_to_500'] = 0
+          distributions['501_to_1000'] = 0
+          distributions['1001_to_5000'] = 0
+          distributions['5001_to_10000'] = 0
+          distributions['10001_to_inf'] = 0
+          for obj in reader:
+               if obj['score'] <= -101:
+                    distributions['-101_to_-inf'] += 1
+               elif obj['score'] >= -100 and obj['score'] <= -21:
+                    distributions['-100_to_-21'] += 1
+               elif obj['score'] >= -20 and obj['score'] <= -11:
+                    distributions['-20_to_-11'] += 1
+               elif obj['score'] >= -10 and obj['score'] <= -6:
+                    distributions['-10_to_-6'] += 1
+               elif obj['score'] >= -5 and obj['score'] <= -1:
+                    distributions['-5_to_-1'] += 1
+               elif obj['score'] == 0:
+                    distributions['0'] += 1
+               elif obj['score'] == 1:
+                    distributions['1'] += 1
+               elif obj['score'] == 2:
+                    distributions['2'] += 1
+               elif obj['score'] == 3:
+                    distributions['3'] += 1
+               elif obj['score'] == 4:
+                    distributions['4'] += 1
+               elif obj['score'] == 5:
+                    distributions['5'] += 1
+               elif obj['score'] >= 6 and obj['score'] <= 10:
+                    distributions['6_to_10'] += 1
+               elif obj['score'] >= 11 and obj['score'] <= 50:
+                    distributions['11_to_50'] += 1
+               elif obj['score'] >= 51 and obj['score'] <= 100:
+                    distributions['51_to_100'] += 1
+               elif obj['score'] >= 101 and obj['score'] <= 500:
+                    distributions['101_to_500'] += 1
+               elif obj['score'] >= 501 and obj['score'] <= 1000:
+                    distributions['501_to_1000'] += 1
+               elif obj['score'] >= 1001 and obj['score'] <= 5000:
+                    distributions['1001_to_5000'] += 1
+               elif obj['score'] >= 1001 and obj['score'] <= 5000:
+                    distributions['1001_to_5000'] += 1
+               elif obj['score'] >= 5001 and obj['score'] <= 10000:
+                    distributions['5001_to_10000'] += 1
+               elif obj['score'] >= 10001:
+                    distributions['10001_to_inf'] += 1
+          writer.write(distributions)
 
 #Example of how to load df from jsonlines
 #df = pd.read_json('reddit_data_small.json', lines=True)
 
 #split_data('D:\Datasets\RC_2015-01.json', 'D:\Datasets\\reddit_data')
 
-create_dataset_small('D:\Datasets\\reddit_data_part_0.json', 'reddit_data_small.json', 100000)
+#create_dataset_small('D:\Datasets\\reddit_data_part_0.json', 'reddit_data_small.json', 100000)
+
+get_score_distribution("reddit_data_small.json", "reddit_data_small_distribution.json")
